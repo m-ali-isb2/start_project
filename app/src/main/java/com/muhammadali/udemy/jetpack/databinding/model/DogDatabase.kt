aed6642c711de8c1935dcbdb4df2eb85d7b20c76
timesacoAli <44408196+timesacoAli@users.noreply.github.com>
2020-06-15 21:18:16 +0500

    Initial commit

---
+++ app/src/main/java/com/muhammadali/udemy/jetpack/databinding/model/DogDatabase.kt
@@ -0,0 +1,32 @@
+package com.muhammadali.udemy.jetpack.databinding.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Created by Muhammad Ali on 17-May-20.
+ * Email muhammad.ali9385@gmail.com
+ */
+@Database(entities = [DogBreed::class], version = 1)
+abstract class DogDatabase : RoomDatabase() {
+    abstract fun dogDao(): DogDao
+
+    companion object {
+        @Volatile
+        private var instance: DogDatabase? = null
+        private var lock = Any()
+
+
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, DogDatabase::class.java, "DogDatabase2")
+                .build()
+    }
+}
---
+++ app/src/main/java/com/muhammadali/udemy/jetpack/databinding/views/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package com.muhammadali.udemy.jetpack.databinding.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.preference.PreferenceFragmentCompat
+import com.muhammadali.udemy.jetpack.databinding.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+}
